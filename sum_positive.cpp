c5b10af16be0766df366054637c30111d7be729d
JustinFengWu <a1891628@adelaide.edu.au>
2023-07-24 13:45:21 +0930

    workshop 1

---
+++ sum_positive.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+extern int main()
+// Sums together all positive numbers and returns them
+int sum_positive(int array[], int n) {
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        if (array[i] > 0) {
+            total += array[i];
+        }
+    }
+
+    return total;
+}
