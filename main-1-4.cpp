94a78243fa31b0c35e991c7f86549c231bbbe9e3
JustinFengWu <a1891628@adelaide.edu.au>
2023-08-01 00:21:19 +0930

    changes to 1-4

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,13 +1,10 @@
+#include <iostream>
+
 void print_scaled(int[3][3], int);
 
 int main() {
-    int matrix[3][3] = {
-        {1, 2, 3},
-        {5, 6, 7},
-        {9, 1, 1},
-    };
-
-    print_scaled(matrix, 3);
-
+    int scale = 3;
+    int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    print_scaled(matrix, scale);
     return 0;
 }
