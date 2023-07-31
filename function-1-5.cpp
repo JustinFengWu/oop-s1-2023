e3e11718380c6f8f49bfaf194895b49c6ac38b13
JustinFengWu <a1891628@adelaide.edu.au>
2023-07-31 22:13:39 +0930

    finished question 1-5

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -6,7 +6,8 @@ void print_summed(int array1[3][3],int array2[3][3]) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             temp = array1[i][j] + array2[i][j];
-
+            std::cout << temp << " ";
         }
+        std::cout << std::endl;
     }
 }
--- main-1-5.cpp
+++ main-1-5.cpp
@@ -1,1 +1,17 @@
-#include 
+void print_summed(int[3][3], int [3][3]);
+
+int main() {
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+    int matrix2[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+
+    print_summed(matrix, matrix2);
+
+}
