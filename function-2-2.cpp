b4cf8a0eb231b8f807c7ec8b24ea5cd36b7b6887
JustinFengWu <a1891628@adelaide.edu.au>
2023-07-31 23:13:52 +0930

    finished question-2-2

---
+++ function-2-2.cpp
@@ -0,0 +1,12 @@
+#include <math.h>
+
+int binary_to_int(int binary_digits[], int number_of_digits) {
+    int power = number_of_digits;
+    int sum = 0;
+    for (int i = 0; i < number_of_digits; i++) {
+        sum += pow(2, power)*binary_digits[i];
+        power--;
+    }
+
+    return sum;
+}
---
+++ main-2-2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int binary_to_int(int[6], int);
+
+int main() {
+    int binary[6] = {0, 1, 0, 1, 0, 1};
+    std::cout << binary_to_int(binary, 6) << std::endl;
+}
