e2ba758b61f6421cc9b6b64977063d769bf1a299
JustinFengWu <a1891628@adelaide.edu.au>
2023-08-23 15:24:13 +0930

    first

---
+++ Unit.h
@@ -0,0 +1,20 @@
+#ifndef UNIT_H
+#define UNIT_H
+
+class Unit:
+private {
+    int unit_val;
+    int num_beds;
+    double unit_size;
+} 
+public {
+    Unit();
+    Unit(int unit_val, int num_beds, double unit_size);
+    int get_number_bedrooms();
+    int get_value();
+    double get_area();
+    
+}
+
+#endif
+
