ad2d557cc4314ec5edc25ddd0a683fca968b19ed
JustinFengWu <a1891628@adelaide.edu.au>
2023-09-06 14:15:03 +0930

    few changes

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -6,6 +6,10 @@
 
 int main() {
     TV tv1(10, 100);
+    // it is very important to understand that you don't need a pointer when allocating objects statically, expecially singular objects. 
+    // you only need pointers that points towards dynamically allocated memory, whether it be arrays or just a singular object.
+    // pointers points to what it is assigned to, hence storing the address. to dereference it (uncover that address being stored) use the &sign.
+    // if function takes in pointer as parameter, pointer->pointer or &object->pointer. 
     TV tv2(10, 100);
     Fridge f1(10, 100);
     Fridge f2(10, 100);
